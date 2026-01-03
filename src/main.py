a38ab0b68b9b01c1f00b77baa8d0e55f3f8f14f0
not-ETHICAL-HACKER <johanwarrior0@gmail.com>
2026-01-03 17:04:05 +0530

    Refactor ternary logic and add alignment function
    Introduces an align() function to pad ternary strings, refactors Ternary to inherit from str, adds OR logic, and updates conversion and OR methods for improved ternary operations.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,34 @@
-class Ternary:
+def align(a: str, b: str) -> tuple[str, str]:
+    while len(a) != len(b):
+        if len(a) > len(b):
+            b = "0" + b
+        elif len(a) < len(b):
+            a = "0" + a
+        else:
+            return a, b
+    return a, b
+
+
+class Ternary(str):
     def __init__(self, num: int) -> None:
         self.num: int = num
         self.pos = "+"
         self.trits: dict[int, str] = {-1: "-", 0: "0", 1: "+"}
-    def __repr__(self)->str:
+        self.OR = {
+    ('+', '+'): '+', ('+', '0'): '+', ('+', '-'): '0',
+    ('0', '+'): '+', ('0', '0'): '0', ('0', '-'): '-',
+    ('-', '+'): '0', ('-', '0'): '-', ('-', '-'): '-',
+}
+
+    def __repr__(self) -> str:
         return self.trits[self.num]
-    def Convert(self)->str:
-        final=""
-        n=self.num
+
+    def Convert(self) -> str:
+        final = "0t"
+        n = self.num
         if n == 0:
             return "0"
-        while n!= 0:
+        while n != 0:
             r = n % 3
             n = n // 3
 
@@ -18,16 +36,26 @@ class Ternary:
                 r = -1
                 n = n + 1
 
-            final+=self.trits[r]
+            final += self.trits[r]
         return final[::-1]
 
+
 class ternary(Ternary):
-    def convert(self) -> Ternary|str:
-        return Ternary(self.num).Convert()
+    def convert(self) -> Ternary | str:
+        self.ternary = Ternary(self.num).Convert()
+        return self.ternary
 
+    def __or__(self, other: Ternary | int) -> Ternary:
+        a = self.ternary
+        b = Ternary(other).Convert()
+        a, b = align(a=a, b=b)
+        a = [sign*3**i for i, sign in enumerate(a[::-1])]
+        b = [sign*3**i for i, sign in enumerate(b[::-1])]
 
-    def T_OR(self, other: Ternary) -> Ternary:
-        pass
+    def T_OR(self, other: str) -> Ternary | str:
+        other = other[2:]
+        self.ternary = self.ternary[2:]
+        return max(other, self.ternary)
 
 
 print(ternary(50).convert())
